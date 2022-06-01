d55d7bfa6a0018e9744c472a7dc57ebdb876c833
Maksym Seliukov <MS20644@america.gds.panasonic.com>
2022-06-01 17:46:53 -0500

    started new python project

---
+++ tests/reverse_order.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        input = "Roses are red and I am glad"
+        expected_result = "glad am I red are Roses"
+
+        output = self.reverse_string(input)
+        self.assertEqual(expected_result, output)  # add assertion here
+
+    def reverse_string(self, input: str):
+        return ' '.join(list(reversed(input.split())))
+
+if __name__ == '__main__':
+    unittest.main()
